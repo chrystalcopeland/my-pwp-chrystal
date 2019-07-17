5b3782bead6e35b2be0c7b97033dd2a258e8f4fa
Chrystal Copeland <canderson73@cnm.edu>
2019-07-17 01:02:28 -0600

    Rough draft of Goals and Persona Added

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Chrystal Copeland PWP</title>
+		<link rel="stylesheet" href="./style.css"/>
+		<script src="./script.js"></script>
+	</head>
+	<body>
+		<H1>Chrystal Copeland </H1>
+		<h2>
+			Purpose
+		</h2>
+		<p>
+			To get a client! I am so excited to put what I am learning into practice. I can't wait to use what I am learning
+			to create beautiful and powerful websites for my clients.
+		</p>
+		<h2>
+			Audience
+		</h2>
+		<p>
+			I will be displaying this site as an example of my coding work to prospective clients and potential employers.
+			My primary audience will be women owned businesses for freelance graphic design work or for a woman owned business.
+		</p>
+		<h2>
+			Goal
+		</h2>
+		<p>
+			<ul>
+			<li>My goal is to attract freelance clients.</li>
+			<li>I will be able to use this site to serve as a gallery to showcase and promote my work.</li>
+			<li>I also want to increase the reach of my social media presence.</li>
+		</ul>
+		</p>
+		<h2>
+			Persona
+		</h2>
+		<p>Name: Devonna</p>
+		<p>Age: 35</p>
+		<p>Occupation: Self employed and her own boutique hand crafted wedding stationary shop. She has graphic design
+			background and 2 employees who are graphic artists. She currently has a website in Square Space, but wants to
+			create a site where clients can interactively create wedding invitations online to boost sales. She is looking for
+			a freelance web designer to start, but in the long term, this person could become an employee if the website is able
+			to generate revenue to sustain a full time web-developer on staff.
+		</p>
+		<p>Devonna and I met at a networking event for female entrepeneurs who want to support other women owned businesses.
+		She specifically wants to work with a woman to create her new website, but doesn't have the money to hire a full time
+		web developer for what she has in mind. </p>
+		<p>
+			Devonna's biggest frustration is getting clients to understand the time involved in custom stationary for weddings.
+			She would like to use the website to offer both high end stationary options, but also allow some customers who want
+			an affordable option to "create their own" stationary. She may even create a separate company identity for lower
+			end stationary design to help boost revenue but not detract from her high end brand she has already developed.
+		</p>
+
+
+	</body>
+</html>
+
+
