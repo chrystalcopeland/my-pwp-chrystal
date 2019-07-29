811111dc555aafdaf2df487f9aa8aa0bd1ae86ae
Chrystal Copeland <canderson73@cnm.edu>
2019-07-29 16:59:48 -0600

    added information for milestone 2a

---
+++ public_html/documentation/milestone-2a.php
@@ -0,0 +1,50 @@
+<h1>
+	Content Strategy
+</h1>
+
+<p>
+	My site will be one page and will consist of the following sections:
+</p>
+
+<h2>
+	Welcome
+</h2>
+<p>
+	Primarily graphic design focused- introducing the name of the business "Forever and a Dahlia Design"- wanting to spend
+	most of my time developing this website and learning to style it with CSS. I am hoping to use minimal text to highlight design.
+</p>
+<h2>
+	About
+</h2>
+<p>
+	a short paragraph about forever and a dahlia- creating and designing and developing websites
+</p>
+<h2>
+	Portfolio
+</h2>
+<p>
+	Pictures with links to some of my best websites indicated by the logo I created for
+	the website, will include my personal website and capstone project.
+</p>
+<h2>
+	Contact
+</h2>
+<h2>
+	An additional section with more text- possibly about what clients can expect about the design process, or a bit of text that will
+	market directly to my persona- wanting to attract design oriented and creative individuals.
+</h2>
+<p>
+	a contact box for clients to be able to reach me.
+</p>
+
+
+<h2>
+	Mobile Wireframe
+</h2>
+<img src="wireframe-mobile.jpg" alt="Mobile Wireframe" />
+
+<h2>
+	Desktop Wireframe
+</h2>
+<img src="wireframe-desktop.jpg" width="600", height="1205" alt="Desktop Wireframe" />
+
