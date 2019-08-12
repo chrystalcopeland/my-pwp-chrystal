425380f372382d8610717fc72d6bed4e2ebad5ad
Chrystal Copeland <canderson73@cnm.edu>
2019-08-11 20:24:46 -0600

    updated bootstrap code for better layout

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,7 @@
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+<!--
 		<style>
 			body, html {
 				height: 100%;
@@ -25,7 +25,7 @@
 			}
 		</style>
 	</head>
-	<body>
+	</body>
 
 		<div class="bg"></div>
 
@@ -36,61 +36,73 @@
 		<!-- FontAwesome -->
 		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
 		<title>Chrystal Copeland</title>
-	</head>
-	<body>
+		</head>
+		<body>
 
-		<!-- Optional JavaScript -->
-		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-				  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-				  crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-				  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-				  crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-				  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-				  crossorigin="anonymous"></script>
+			<!-- Optional JavaScript -->
+			<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+					  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+					  crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+					  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+					  crossorigin="anonymous"></script>
+			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+					  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+					  crossorigin="anonymous"></script>
 
-		<!--Nav bar-->
-		<header>
-			<nav class="navbar navbar-expand-md navbar-light bg-light d-flex justify-content-end">
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav">
-						<li class="nav-item active">
-							<a class="nav-link" href="#about">About</a>
-						</li>
-						<li class="nav-item active">
-							<a class="nav-link" href="#testimonials">Testimonials</a>
-						</li>
-						<li class="nav-item active">
-							<a class="nav-link" href="#portfolio">Portfolio</a>
-						</li>
-						<li class="nav-item active">
-							<a class="nav-link" href="#contact-section">Contact</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</header>
-		<!--Hero image and welcome message-->
-		<section>
-			<div class="container-fluid backgroundimg" >
-				<div class="row-fluid">
-					<div class="column">
-						<div class="container pt-5">
-							<h1 class="display-3 text-center font-weight-bold">Chrystal Copeland</h1>
-							<p class="lead text-center">Web Development and Marketing</p>
+			<!--Nav bar-->
+			<header>
+				<nav class="navbar sticky-top navbar-light bg-light">
+					<a class="navbar-brand" href="#">Sticky top</a>
+				<nav class="navbar navbar-expand-md navbar-light bg-light d-flex justify-content-end">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+							  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<h2 class="navbar-brand brand-name">
+						<a href="index.html"><img class="img-responsive2"
+														  src="">DigitalPond</a>
+					</h2>
+					<div class="collapse navbar-collapse" id="navbarNav">
+						<ul class="navbar-nav">
+							<li class="nav-item active">
+								<a class="nav-link" href="#about">About</a>
+							</li>
+							<li class="nav-item active">
+								<a class="nav-link" href="#another-link-here">Neat!</a>
+							</li>
+							<li class="nav-item active">
+								<a class="nav-link" href="#portfolio">Portfolio</a>
+							</li>
+							<li class="nav-item active">
+								<a class="nav-link" href="#contact-section">Contact</a>
+							</li>
+						</ul>
+					</div>
+
+				</nav>
+			</header>
+			<!--Hero image and welcome message-->
+				<div class="container-fluid backgroundimg">
+					<div class="row-fluid">
+						<div class="column">
+							<div class="container pt-5">
+								<h1 class="display-3 text-center font-weight-bold">Chrystal Copeland</h1>
+								<p class="lead text-center">Web Development and Marketing</p>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+
 
 			<!-- Jumbotron -->
+			<!--
 			<section class="py-5">
 				<div class="container">
 					<div class="jumbotron">
@@ -103,73 +115,120 @@
 					</div>
 				</div>
 			</section>
-
-		<section class="bg-secondary py-5">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-3">
-						<img class="img-fluid d-block mx-auto" src="http://placekitten.com/200/200" alt="cute kitten">
-					</div>
-					<div class="col-sm-9">
-						<p class="lead">Destroy the blinds scoot butt on the rug. Why must they do that spend six hours per day washing, but still
-							have a crusty butthole. Spot something, big eyes, big eyes, crouch, shake butt, prepare to pounce reward the
-							chosen human with a slow blink. Bathe private parts with tongue then lick owner's face kitty pounce,
-							trip, faceplant you didn't see that no you didn't definitely didn't lick, lick, lick, and preen away the
-							embarrassment. 𝕄𝔼𝕆𝕎 cat is love, cat is life. Attack feet if it fits, i sits so making sure that fluff gets
-							into the owner's eyes or unwrap toilet paper for rub against owner because nose is wet.
-					</div>
-				</div>
-			</div>
-		</section>
-		</div>
-		<!-- portfolio images -->
-		<section class="py-5">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-3 col-sm-6">
-						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="http://placekitten.com/g/200/200" alt="here kitty">
-						<div class="card mb-4">
-							<div class="card-body">
-								<h5 class="card-title">Card Title</h5>
-								<p class="card-text">Annoy the old grumpy cat, start a fight and then retreat to wash when i lose find a way to fit in tiny box.
-									Refuse to leave cardboard box stare at ceiling light for and sometimes switches in french and say
-									"miaou".</p>
-							</div>
+			-->
+<!--About Section-->
+			<section class="bg-secondary py-5">
+				<div class="container" id="about">
+					<div class="row">
+						<div class="col-sm-3">
+							<img class="img-fluid d-block mx-auto"
+								  src="documentation/images/Chrystal%20Copeland%20Headshot.jpg" alt="Chrystal Copeland">
 						</div>
-					</div>
-					<div class="col-lg-3 col-sm-6">
-						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="http://placekitten.com/g/200/200" alt="here kitty">
-						<div class="card mb-4">
-							<div class="card-body">
-								<h5 class="card-title">Card Title</h5>
-								<p class="card-text">Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot,
-									hairball run catnip eat the grass sniff as lick i the shoes for run at 3am my cat stared at me.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-sm-6">
-						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="http://placekitten.com/g/200/200" alt="here kitty">
-						<div class="card mb-4">
-							<div class="card-body">
-								<h5 class="card-title">Card Title</h5>
-								<p class="card-text">Meow rub face on everything i could pee on this if i had the energy and get scared by sudden appearance of
-									cucumber. Murf pratt ungow ungow jump off balcony, onto stranger's head yet russian blue.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-sm-6">
-						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="http://placekitten.com/g/200/200" alt="here kitty">
-						<div class="card mb-4">
-							<div class="card-body">
-								<h5 class="card-title">Card Title</h5>
-								<p class="card-text">Eat half my food and ask for more sniff sniff i shredded your linens for you yet run up and down stairs, shove
-									bum in owner's face like camera lens i will ruin the couch with my claws.</p>
-							</div>
+						<div class="col-sm-9">
+							<p class="lead">Cupcake ipsum dolor. Sit amet marshmallow topping cheesecake muffin. Halvah
+								croissant
+								candy canes bonbon candy. Apple pie jelly beans topping carrot cake danish tart cake cheesecake.
+								Muffin danish chocolate soufflé pastry icing bonbon oat cake.
+								Powder cake jujubes oat cake. Lemon drops tootsie roll marshmallow halvah carrot cake.
 						</div>
 					</div>
 				</div>
+			</section>
 			</div>
-		</section>
+			<!-- portfolio images -->
+			<!-- ABQ Bikes -->
+			<section class="py-5">
+				<div class="container" id="portfolio">
+					<div class="row">
+						<div class="col-lg-3 col-sm-6">
+							<img class="img-fluid rounded d-block mx-auto mb-4"
+								  src="documentation/images/bike%20abq.png" alt="ABQ Bike Trails">
+							<div class="card mb-4">
+								<div class="card-body">
+									<h5 class="card-title">ABQ Bike Trails</h5>
+									<p class="card-text">Bike trails resource built from scratch in a collaborative effort using
+										Agile programming in LAMP stack. </p>
+								</div>
+							</div>
+						</div>
+						<!-- Tree of Life Massage -->
+						<div class="col-lg-3 col-sm-6">
+							<img class="img-fluid rounded d-block mx-auto mb-4"
+								  src="documentation/images/tree-of-life.jpg" alt="Tree of Life Massage">
+							<div class="card mb-4">
+								<div class="card-body">
+									<h5 class="card-title">Tree of Life Massage</h5>
+									<p class="card-text">Health and Wellness website for Tree of Life Massage using WIX.</p>
+								</div>
+							</div>
+						</div>
+						<!-- ABQ Pregnancy -->
+						<div class="col-lg-3 col-sm-6">
+							<img class="img-fluid rounded d-block mx-auto mb-4"
+								  src="documentation/images/abq-pregnancy.png" alt="ABQ pregnancy">
+							<div class="card mb-4">
+								<div class="card-body">
+									<h5 class="card-title">ABQpregnancy.com</h5>
+									<p class="card-text">Resource directory website built using Wordpress and Divi theme.</p>
+								</div>
+							</div>
+						</div>
+						<!-- Youth Jazz Collaborative -->
+						<div class="col-lg-3 col-sm-6">
+							<img class="img-fluid rounded d-block mx-auto mb-4"
+								  src="documentation/images/youth-jazz.jpg" alt="Youth Jazz Collaborative">
+							<div class="card mb-4">
+								<div class="card-body">
+									<h5 class="card-title">Youth Jazz Collborative</h5>
+									<p class="card-text">Rebranding and new website for small non-profit in New Mexico using
+										Squarespace.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- Contact -->
+			<section class="bg-secondary py-5">
+				<div class="container" id="contact-section">
+					<form>
+						<div class="row">
+							<div class="col">
+								<input type="text" class="form-control" placeholder="First name">
+							</div>
+							<div class="col">
+								<input type="text" class="form-control" placeholder="Last name">
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="exampleFormControlInput1">Email address</label>
+							<input type="email" class="form-control" id="exampleFormControlInput1"
+									 placeholder="name@example.com">
+						</div>
+				</div>
+				<div class="container" id="contact-section">
+					<div class="form-group">
+						<label for="exampleFormControlTextarea1">Example textarea</label>
+						<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+					</div>
+					<button type="button" class="btn btn-outline-light">Submit</button>
+					</form>
+				</div>
+			</section>
+			<!--Bootstrap sticky footer-->
+			<footer>
+				<div class="container-fluid">
+					<div class="sticky-footer text-center text-white bg-dark pt-2 pb-0 m-0 fixed-bottom">
+						<p>
+							<a href="https://github.com/chrystalcopeland/" target="_blank"><i class="fab fa-github fa-2x"></i></a>
+							<a href="https://www.linkedin.com/in/copelandchrystal/" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a>
+							<a href="https://twitter.com/chryscopeland/" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
+							<br>
+							Chrystal Copeland ©2019
+						</p>
+					</div>
+				</div>
+			</footer>
 
 
 		</body>
