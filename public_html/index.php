38d2703b6e414a05a37ebec13df707d1f98078d0
Chrystal Copeland <canderson73@cnm.edu>
2019-08-11 21:12:05 -0600

    added logo

--- public_html/index.php
+++ public_html/index.php
@@ -4,30 +4,31 @@
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<!--
-		<style>
-			body, html {
-				height: 100%;
-				margin: 0;
-			}
+		<!--
+				<style>
+					body, html {
+						height: 100%;
+						margin: 0;
+					}
 
-			.bg {
-				/* The image used */
-				background-image: url("documentation/images/background-dahlia.jpg");
+					.bg {
+						/* The image used */
+						background-image: url("documentation/images/background-dahlia.jpg");
 
-				/* Full height */
-				height: 100%;
+						/* Full height */
+						height: 100%;
 
-				/* Center and scale the image nicely */
-				background-position: center;
-				background-repeat: no-repeat;
-				background-size: cover;
-			}
-		</style>
-	</head>
-	</body>
+						/* Center and scale the image nicely */
+						background-position: center;
+						background-repeat: no-repeat;
+						background-size: cover;
+					}
+				</style>
+			</head>
+			</body>
 
-		<div class="bg"></div>
+				<div class="bg"></div>
+				-->
 
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
@@ -40,25 +41,23 @@
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 
 		<title>Chrystal Copeland</title>
-		</head>
-		<body>
+	</head>
+	<body>
 
-			<!-- Optional JavaScript -->
-			<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-					  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-					  crossorigin="anonymous"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-					  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-					  crossorigin="anonymous"></script>
-			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-					  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-					  crossorigin="anonymous"></script>
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+				  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+				  crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+				  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+				  crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+				  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+				  crossorigin="anonymous"></script>
 
-			<!--Nav bar-->
-			<header>
-				<nav class="navbar sticky-top navbar-light bg-light">
-					<a class="navbar-brand" href="#">Sticky top</a>
+		<!--Nav bar-->
+		<header>
 				<nav class="navbar navbar-expand-md navbar-light bg-light d-flex justify-content-end">
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
 							  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -66,16 +65,18 @@
 					</button>
 					<h2 class="navbar-brand brand-name">
 						<a href="index.html"><img class="img-responsive2"
-														  src="">DigitalPond</a>
+														  src="documentation/images/dahlia-grey.png" height="50 px" width="50 px"></a>
 					</h2>
 					<div class="collapse navbar-collapse" id="navbarNav">
 						<ul class="navbar-nav">
 							<li class="nav-item active">
 								<a class="nav-link" href="#about">About</a>
 							</li>
+							<!--
 							<li class="nav-item active">
-								<a class="nav-link" href="#another-link-here">Neat!</a>
+								<a class="nav-link" href="#text-link-here">Text</a>
 							</li>
+							-->
 							<li class="nav-item active">
 								<a class="nav-link" href="#portfolio">Portfolio</a>
 							</li>
@@ -86,150 +87,158 @@
 					</div>
 
 				</nav>
-			</header>
-			<!--Hero image and welcome message-->
-				<div class="container-fluid backgroundimg">
-					<div class="row-fluid">
-						<div class="column">
-							<div class="container pt-5">
-								<h1 class="display-3 text-center font-weight-bold">Chrystal Copeland</h1>
-								<p class="lead text-center">Web Development and Marketing</p>
-							</div>
-						</div>
+		</header>
+		<!--Hero image and welcome message-->
+		<div class="container-fluid backgroundimg">
+			<div class="row-fluid">
+				<div class="column">
+					<div class="container pt-5">
+						<h1 class="display-3 text-center font-weight-bold">Chrystal Copeland</h1>
+						<p class="lead text-center">Web Development and Marketing</p>
 					</div>
 				</div>
-			</section>
-
-
-			<!-- Jumbotron -->
-			<!--
-			<section class="py-5">
-				<div class="container">
-					<div class="jumbotron">
-						<img src="documentation/images/background-dahlia.jpg" class="img-fluid" alt="Responsive image">
-						<h1 class="display-4">Chrystal Copeland</h1>
-						<p class="lead">Web Development and Marketing</p>
-						<hr class="my-4">
-						<p>Custom Websites and Marketing for Wellness and Natural Lifestyle Business Solutions</p>
-						<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-					</div>
-				</div>
-			</section>
-			-->
-<!--About Section-->
-			<section class="bg-secondary py-5">
-				<div class="container" id="about">
-					<div class="row">
-						<div class="col-sm-3">
-							<img class="img-fluid d-block mx-auto"
-								  src="documentation/images/Chrystal%20Copeland%20Headshot.jpg" alt="Chrystal Copeland">
-						</div>
-						<div class="col-sm-9">
-							<p class="lead">Cupcake ipsum dolor. Sit amet marshmallow topping cheesecake muffin. Halvah
-								croissant
-								candy canes bonbon candy. Apple pie jelly beans topping carrot cake danish tart cake cheesecake.
-								Muffin danish chocolate soufflé pastry icing bonbon oat cake.
-								Powder cake jujubes oat cake. Lemon drops tootsie roll marshmallow halvah carrot cake.
-						</div>
-					</div>
-				</div>
-			</section>
 			</div>
-			<!-- portfolio images -->
-			<!-- ABQ Bikes -->
-			<section class="py-5">
-				<div class="container" id="portfolio">
-					<div class="row">
-						<div class="col-lg-3 col-sm-6">
-							<img class="img-fluid rounded d-block mx-auto mb-4"
-								  src="documentation/images/bike%20abq.png" alt="ABQ Bike Trails">
-							<div class="card mb-4">
-								<div class="card-body">
-									<h5 class="card-title">ABQ Bike Trails</h5>
-									<p class="card-text">Bike trails resource built from scratch in a collaborative effort using
-										Agile programming in LAMP stack. </p>
-								</div>
+		</div>
+		</section>
+
+
+		<!-- Jumbotron -->
+		<!--
+		<section class="py-5">
+			<div class="container">
+				<div class="jumbotron">
+					<img src="documentation/images/background-dahlia.jpg" class="img-fluid" alt="Responsive image">
+					<h1 class="display-4">Chrystal Copeland</h1>
+					<p class="lead">Web Development and Marketing</p>
+					<hr class="my-4">
+					<p>Custom Websites and Marketing for Wellness and Natural Lifestyle Business Solutions</p>
+					<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+				</div>
+			</div>
+		</section>
+		-->
+		<!--About Section-->
+		<section class="bg-secondary py-5">
+			<div class="container" id="about">
+				<h2>About</h2>
+				<div class="row">
+					<div class="col-sm-3">
+						<img class="img-fluid d-block mx-auto"
+							  src="documentation/images/chrystal-copeland-headshot.jpg" alt="Chrystal Copeland">
+					</div>
+					<div class="col-sm-9">
+						<p class="lead">Cupcake ipsum dolor. Sit amet marshmallow topping cheesecake muffin. Halvah
+							croissant
+							candy canes bonbon candy. Apple pie jelly beans topping carrot cake danish tart cake cheesecake.
+							Muffin danish chocolate soufflé pastry icing bonbon oat cake.
+							Powder cake jujubes oat cake. Lemon drops tootsie roll marshmallow halvah carrot cake.
+					</div>
+				</div>
+			</div>
+		</section>
+		</div>
+		<!-- portfolio images -->
+		<!-- ABQ Bikes -->
+		<section class="py-5">
+			<div class="container" id="portfolio">
+				<h2>Portfolio</h2>
+				<div class="row">
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid rounded d-block mx-auto mb-4"
+							  src="documentation/images/bike%20abq.png" alt="ABQ Bike Trails">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">ABQ Bike Trails</h5>
+								<p class="card-text">Bike trails resource built from scratch in a collaborative effort using
+									Agile programming in LAMP stack. </p>
 							</div>
 						</div>
-						<!-- Tree of Life Massage -->
-						<div class="col-lg-3 col-sm-6">
-							<img class="img-fluid rounded d-block mx-auto mb-4"
-								  src="documentation/images/tree-of-life.jpg" alt="Tree of Life Massage">
-							<div class="card mb-4">
-								<div class="card-body">
-									<h5 class="card-title">Tree of Life Massage</h5>
-									<p class="card-text">Health and Wellness website for Tree of Life Massage using WIX.</p>
-								</div>
+					</div>
+					<!-- Tree of Life Massage -->
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid rounded d-block mx-auto mb-4"
+							  src="documentation/images/tree-of-life.jpg" alt="Tree of Life Massage">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Tree of Life Massage</h5>
+								<p class="card-text">Health and Wellness website for Tree of Life Massage using WIX.</p>
 							</div>
 						</div>
-						<!-- ABQ Pregnancy -->
-						<div class="col-lg-3 col-sm-6">
-							<img class="img-fluid rounded d-block mx-auto mb-4"
-								  src="documentation/images/abq-pregnancy.png" alt="ABQ pregnancy">
-							<div class="card mb-4">
-								<div class="card-body">
-									<h5 class="card-title">ABQpregnancy.com</h5>
-									<p class="card-text">Resource directory website built using Wordpress and Divi theme.</p>
-								</div>
+					</div>
+					<!-- ABQ Pregnancy -->
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid rounded d-block mx-auto mb-4"
+							  src="documentation/images/abq-pregnancy.png" alt="ABQ pregnancy">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">ABQpregnancy.com</h5>
+								<p class="card-text">Resource directory website built using Wordpress and Divi theme.</p>
 							</div>
 						</div>
-						<!-- Youth Jazz Collaborative -->
-						<div class="col-lg-3 col-sm-6">
-							<img class="img-fluid rounded d-block mx-auto mb-4"
-								  src="documentation/images/youth-jazz.jpg" alt="Youth Jazz Collaborative">
-							<div class="card mb-4">
-								<div class="card-body">
-									<h5 class="card-title">Youth Jazz Collborative</h5>
-									<p class="card-text">Rebranding and new website for small non-profit in New Mexico using
-										Squarespace.</p>
-								</div>
+					</div>
+					<!-- Youth Jazz Collaborative -->
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid rounded d-block mx-auto mb-4"
+							  src="documentation/images/youth-jazz.jpg" alt="Youth Jazz Collaborative">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Youth Jazz Collborative</h5>
+								<p class="card-text">Rebranding and new website for small non-profit in New Mexico using
+									Squarespace.</p>
 							</div>
 						</div>
 					</div>
 				</div>
-			</section>
-			<!-- Contact -->
-			<section class="bg-secondary py-5">
-				<div class="container" id="contact-section">
-					<form>
-						<div class="row">
-							<div class="col">
-								<input type="text" class="form-control" placeholder="First name">
+			</div>
+		</section>
+		<!-- Contact -->
+		<secgtion class="bg-secondary py-5">
+			<div class="container" id="contact-section">
+				<div class="row ml-1">
+					<div class="column pt-5 mb-5 pb-5 col-12">
+						<h2>Contact</h2>
+						<form>
+							<div class="row">
+								<div class="col">
+									<input type="text" class="form-control" placeholder="First name">
+								</div>
+								<div class="col">
+									<input type="text" class="form-control" placeholder="Last name">
+								</div>
 							</div>
-							<div class="col">
-								<input type="text" class="form-control" placeholder="Last name">
+							<div class="form-group">
+								<label for="exampleFormControlInput1">Email address</label>
+								<input type="email" class="form-control" id="exampleFormControlInput1"
+										 placeholder="name@example.com">
 							</div>
-						</div>
+					</div>
+					<div class="container" id="contact-section">
 						<div class="form-group">
-							<label for="exampleFormControlInput1">Email address</label>
-							<input type="email" class="form-control" id="exampleFormControlInput1"
-									 placeholder="name@example.com">
+							<label for="exampleFormControlTextarea1">Message</label>
+							<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 						</div>
-				</div>
-				<div class="container" id="contact-section">
-					<div class="form-group">
-						<label for="exampleFormControlTextarea1">Example textarea</label>
-						<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+						<button type="button" class="btn btn-outline-light">Submit</button>
+						</form>
 					</div>
-					<button type="button" class="btn btn-outline-light">Submit</button>
-					</form>
+		</secgtion>
+		<!--Bootstrap sticky footer-->
+		<footer>
+			<div class="container-fluid">
+				<div class="sticky-footer text-center text-white bg-dark pt-2 pb-0 m-0 fixed-bottom">
+					<p>
+						<a href="https://github.com/chrystalcopeland/" target="_blank"><i
+								class="fab fa-github fa-2x"></i></a>
+						<a href="https://www.linkedin.com/in/copelandchrystal/" target="_blank"><i
+								class="fab fa-linkedin fa-2x"></i></a>
+						<a href="https://twitter.com/chryscopeland/" target="_blank"><i
+								class="fab fa-twitter fa-2x"></i></a>
+						<br>
+						Chrystal Copeland ©2019
+					</p>
 				</div>
-			</section>
-			<!--Bootstrap sticky footer-->
-			<footer>
-				<div class="container-fluid">
-					<div class="sticky-footer text-center text-white bg-dark pt-2 pb-0 m-0 fixed-bottom">
-						<p>
-							<a href="https://github.com/chrystalcopeland/" target="_blank"><i class="fab fa-github fa-2x"></i></a>
-							<a href="https://www.linkedin.com/in/copelandchrystal/" target="_blank"><i class="fab fa-linkedin fa-2x"></i></a>
-							<a href="https://twitter.com/chryscopeland/" target="_blank"><i class="fab fa-twitter fa-2x"></i></a>
-							<br>
-							Chrystal Copeland ©2019
-						</p>
-					</div>
-				</div>
-			</footer>
+			</div>
+		</footer>
 
 
-		</body>
+	</body>
 </html>
