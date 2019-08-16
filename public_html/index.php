f8bceaa2d9df8b572eaf5ecb3ea7db7e46cc2e84
Chrystal Copeland <canderson73@cnm.edu>
2019-08-16 10:25:29 -0600

    added email validations and fixed div closures

--- public_html/index.php
+++ public_html/index.php
@@ -5,6 +5,10 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+		<!-- JQuery Library -->
+		<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+				  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 				integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -26,10 +30,14 @@
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
 				  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
 				  crossorigin="anonymous"></script>
+
 		<!-- jQuery Form, Additional Methods, Validate -->
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
 
 		<!-- Your JavaScript Form Validator -->
 		<script src="js/form-validate.js"></script>
@@ -43,9 +51,9 @@
 		<title>Chrystal Copeland</title>
 	</head>
 	<body>
+		<main>
 
-		<!--Nav bar-->
-		<header>
+			<!--Nav bar-->
 			<nav class="navbar navbar-expand-md navbar-light bg-light d-flex justify-content-end">
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
 						  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -75,206 +83,202 @@
 				</div>
 
 			</nav>
-		</header>
-		<!--Hero image and welcome message-->
-		<div class="container-fluid backgroundimg">
-			<div class="row-fluid">
-				<div class="column">
-					<div class="container pt-5">
-						<h1 class="display-3 text-center font-weight-bold">Chrystal Copeland</h1>
-						<p class="lead text-center">Web Development and Marketing</p>
+			<!--Hero image and welcome message-->
+			<div class="container-fluid backgroundimg">
+				<div class="row-fluid">
+					<div class="column">
+						<div class="container pt-5">
+							<h1 class="display-3 text-center font-weight-bold">Chrystal Copeland</h1>
+							<p class="lead text-center">Web Development and Marketing</p>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-		</section>
+			</section>
 
 
-		<!-- Jumbotron -->
-		<!--
-		<section class="py-5">
-			<div class="container">
-				<div class="jumbotron">
-					<img src="documentation/images/background-dahlia.jpg" class="img-fluid" alt="Responsive image">
-					<h1 class="display-4">Chrystal Copeland</h1>
-					<p class="lead">Web Development and Marketing</p>
-					<hr class="my-4">
-					<p>Custom Websites and Marketing for Wellness and Natural Lifestyle Business Solutions</p>
-					<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-				</div>
-			</div>
-		</section>
-		-->
-		<!--About Section-->
-		<section class="bg-secondary py-5">
-			<div class="container" id="about">
-				<h2>About</h2>
-				<div class="row">
-					<div class="col-sm-3">
-						<img class="img-fluid d-block mx-auto"
-							  src="images/chrystal-copeland-headshot.jpg" alt="Chrystal Copeland">
-					</div>
-					<div class="col-sm-9">
-						<p class="lead">Cupcake ipsum dolor. Sit amet marshmallow topping cheesecake muffin. Halvah
-							croissant
-							candy canes bonbon candy. Apple pie jelly beans topping carrot cake danish tart cake cheesecake.
-							Muffin danish chocolate soufflé pastry icing bonbon oat cake.
-							Powder cake jujubes oat cake. Lemon drops tootsie roll marshmallow halvah carrot cake.
+			<!-- Jumbotron -->
+			<!--
+			<section class="py-5">
+				<div class="container">
+					<div class="jumbotron">
+						<img src="documentation/images/background-dahlia.jpg" class="img-fluid" alt="Responsive image">
+						<h1 class="display-4">Chrystal Copeland</h1>
+						<p class="lead">Web Development and Marketing</p>
+						<hr class="my-4">
+						<p>Custom Websites and Marketing for Wellness and Natural Lifestyle Business Solutions</p>
+						<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 					</div>
 				</div>
-			</div>
-		</section>
-		</div>
-		<!-- portfolio images -->
-		<!-- ABQ Bikes -->
-		<section class="py-5">
-			<div class="container" id="portfolio">
-				<h2>Portfolio</h2>
-				<div class="row">
-					<div class="col-lg-3 col-sm-6">
-						<img class="img-fluid rounded d-block mx-auto mb-4"
-							  src="images/bike%20abq.png" alt="ABQ Bike Trails">
-						<div class="card mb-4">
-							<div class="card-body">
-								<h5 class="card-title">ABQ Bike Trails</h5>
-								<p class="card-text">Bike trails resource built from scratch in a collaborative effort using
-									Agile programming in LAMP stack. </p>
-							</div>
+			</section>
+			-->
+			<!--About Section-->
+			<section class="bg-secondary- py-5">
+				<div class="container" id="about">
+					<h2>About</h2>
+					<div class="row">
+						<div class="col-sm-3">
+							<img class="img-fluid d-block mx-auto"
+								  src="images/chrystal-copeland-headshot.jpg" alt="Chrystal Copeland">
 						</div>
-					</div>
-					<!-- Tree of Life Massage -->
-					<div class="col-lg-3 col-sm-6">
-						<img class="img-fluid rounded d-block mx-auto mb-4"
-							  src="images/tree-of-life.jpg" alt="Tree of Life Massage">
-						<div class="card mb-4">
-							<div class="card-body">
-								<h5 class="card-title">Tree of Life Massage</h5>
-								<p class="card-text">Health and Wellness website for Tree of Life Massage using WIX.</p>
-							</div>
-						</div>
-					</div>
-					<!-- ABQ Pregnancy -->
-					<div class="col-lg-3 col-sm-6">
-						<img class="img-fluid rounded d-block mx-auto mb-4"
-							  src="images/abq-pregnancy.png" alt="ABQ pregnancy">
-						<div class="card mb-4">
-							<div class="card-body">
-								<h5 class="card-title">ABQpregnancy.com</h5>
-								<p class="card-text">Resource directory website built using Wordpress and Divi theme.</p>
-							</div>
-						</div>
-					</div>
-					<!-- Youth Jazz Collaborative -->
-					<div class="col-lg-3 col-sm-6">
-						<img class="img-fluid rounded d-block mx-auto mb-4"
-							  src="images/youth-jazz.jpg" alt="Youth Jazz Collaborative">
-						<div class="card mb-4">
-							<div class="card-body">
-								<h5 class="card-title">Youth Jazz Collborative</h5>
-								<p class="card-text">Rebranding and new website for small non-profit in New Mexico using
-									Squarespace.</p>
-							</div>
+						<div class="col-sm-9">
+							<p class="lead">Cupcake ipsum dolor. Sit amet marshmallow topping cheesecake muffin. Halvah
+								croissant
+								candy canes bonbon candy. Apple pie jelly beans topping carrot cake danish tart cake cheesecake.
+								Muffin danish chocolate soufflé pastry icing bonbon oat cake.
+								Powder cake jujubes oat cake. Lemon drops tootsie roll marshmallow halvah carrot cake.
 						</div>
 					</div>
 				</div>
+			</section>
 			</div>
-		</section>
-		<!-- Contact -->
-		<section class="bg-secondary py-5">
-			<div class="container" id="contact-section">
-				<div class="row ml-1">
+			<!-- portfolio images -->
+			<!-- ABQ Bikes -->
+			<section class="py-5">
+				<div class="container" id="portfolio">
+					<h2>Portfolio</h2>
+					<div class="row">
+						<div class="col-lg-3 col-sm-6">
+							<img class="img-fluid rounded d-block mx-auto mb-4"
+								  src="images/bike%20abq.png" alt="ABQ Bike Trails">
+							<div class="card mb-4">
+								<div class="card-body">
+									<h5 class="card-title">ABQ Bike Trails</h5>
+									<p class="card-text">Bike trails resource built from scratch in a collaborative effort using
+										Agile programming in LAMP stack. </p>
+								</div>
+							</div>
+						</div>
+						<!-- Tree of Life Massage -->
+						<div class="col-lg-3 col-sm-6">
+							<img class="img-fluid rounded d-block mx-auto mb-4"
+								  src="images/tree-of-life.jpg" , alt="Tree of Life Massage">
+							<div class="card mb-4">
+								<div class="card-body">
+									<h5 class="card-title">Tree of Life Massage</h5>
+									<p class="card-text">Health and Wellness website for Tree of Life Massage using WIX.</p>
+								</div>
+							</div>
+						</div>
+						<!-- ABQ Pregnancy -->
+						<div class="col-lg-3 col-sm-6">
+							<img class="img-fluid rounded d-block mx-auto mb-4"
+								  src="images/abq-pregnancy.png" alt="ABQ pregnancy">
+							<div class="card mb-4">
+								<div class="card-body">
+									<h5 class="card-title">ABQpregnancy.com</h5>
+									<p class="card-text">Resource directory website built using Wordpress and Divi theme.</p>
+								</div>
+							</div>
+						</div>
+						<!-- Youth Jazz Collaborative -->
+						<div class="col-lg-3 col-sm-6">
+							<img class="img-fluid rounded d-block mx-auto mb-4"
+								  src="images/youth-jazz.jpg" alt="Youth Jazz Collaborative">
+							<div class="card mb-4">
+								<div class="card-body">
+									<h5 class="card-title">Youth Jazz Collborative</h5>
+									<p class="card-text">Rebranding and new website for small non-profit in New Mexico using
+										Squarespace.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+
+			<!-- Contact Form Container -->
+			<section class="bg-secondary py-5">
+				<div class="container" id="contact-section">
 					<div class="column pt-5 mb-5 pb-5 col-12">
-						<form>
-							<!--
-Begin Sample Contact Form
+						<section class="py-5">
+							<div class="col-lg-6 offset-lg-3">
+								<h2 class="text-center">Contact Chrystal</h2>
 
-Create your own contact form.
-Do not copy and paste this one.
--->
-							<section class="py-5">
-								<div class="container">
-									<div class="row">
-										<div class="col-lg-6 offset-lg-3">
-											<h2 class="text-center">Contact Chrystal</h2>
-											<!--Begin Form -->
-											<form id="contact-form" action="../php/mailer.php" method="post">
-												<div class="form-group">
-													<label for="contactChrystalName">Name <span class="text-danger">*</span></label>
-													<div class="input-group">
-														<div class="input-group-prepend">
-															<div class="input-group-text">
+								<!--Begin Form -->
+								<form id="chrystal-demo-form" action="../php/index.php" method="post">
+									<div class="form-group">
+										<div class="input-group">
+											<div class="input-group-prepend">
+															<span class="input-group-text">
 																<i class="fa fa-user" aria-hidden="true"></i>
-															</div>
-														</div>
-														<input type="text" class="form-control" id="contactChrystal" name="contactChrystal" placeholder="Name">
-													</div>
-												</div>
-												<div class="form-group">
-													<label for="contactChrystalEmail">Email <span class="text-danger">*</span></label>
-													<div class="input-group">
-														<div class="input-group-prepend">
-															<div class="input-group-text">
+															</span>
+											</div>
+											<input type="text" class="form-control" id="name" name="name"
+													 placeholder="Name">
+										</div>
+									</div>
+									<div class="form-group">
+										<label for="email">Email <span class="text-danger">*</span></label>
+										<div class="input-group">
+											<div class="input-group-prepend">
+															<span class="input-group-text">
 																<i class="fa fa-envelope" aria-hidden="true"></i>
-															</div>
-														</div>
-														<input type="email" class="form-control" id="contactChrystalEmail" name="contactChrystalEmail" placeholder="Email">
-													</div>
-												</div>
-												<div class="form-group">
-													<label for="contactChrystalSubject">Subject</label>
-													<div class="input-group">
-														<div class="input-group-prepend">
-															<div class="input-group-text">
-																<i class="fa fa-pencil" aria-hidden="true"></i>
-															</div>
-														</div>
-														<input type="text" class="form-control" id="contactChrystalSubject" name="contactChrystalSubject" placeholder="Subject">
-													</div>
-												</div>
-												<div class="form-group">
-													<label for="contactChrystalMessage">Message <span class="text-danger">*</span></label>
-													<div class="input-group">
-														<div class="input-group-prepend">
-															<div class="input-group-text">
-																<i class="fa fa-comment" aria-hidden="true"></i>
-															</div>
-														</div>
-														<textarea class="form-control" rows="5" id="contactChrystalMessage" name="contactChrystalMessage" placeholder="Message (2000 characters max)"></textarea>
-													</div>
-												</div>
-												<!-- reCAPTCHA -->
-												<div class="g-recaptcha" data-sitekey="--YOUR RECAPTCHA SITE KEY--"></div>
-												<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
-												<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
-											</form>
-											<!--empty area for form error/success output-->
-											<div class="row">
-												<div class="col-xs-12">
-													<div id="output-area"></div>
+															</span>
+											</div>
+											<input type="email" class="form-control" id="email" name="email"
+													 placeholder="Email">
+										</div>
+									</div>
+									<div class="form-group">
+										<label for="subject">Subject</label>
+										<div class="input-group">
+											<div class="input-group-prepend">
+												<div class="input-group-text">
+													<i class="fa fa-pencil" aria-hidden="true"></i>
 												</div>
 											</div>
-										</div><!-- /.col-lg-4 -->
-									</div><!-- /.row -->
-								</div><!--/.container -->
-							</section>
-		<!--Bootstrap sticky footer-->
-		<footer>
-			<div class="container-fluid">
-				<div class="sticky-footer text-center text-white bg-dark pt-2 pb-0 m-0 fixed-bottom">
-					<p>
-						<a href="https://github.com/chrystalcopeland/" target="_blank"><i
-								class="fab fa-github fa-2x"></i></a>
-						<a href="https://www.linkedin.com/in/copelandchrystal/" target="_blank"><i
-								class="fab fa-linkedin fa-2x"></i></a>
-						<a href="https://twitter.com/chryscopeland/" target="_blank"><i
-								class="fab fa-twitter fa-2x"></i></a>
-						<br>
-						Chrystal Copeland ©2019
-					</p>
-				</div>
-			</div>
-		</footer>
+											<input type="text" class="form-control" id="subject" name="subject"
+													 placeholder="Subject">
+										</div>
+									</div>
+									<div class="form-group">
+										<label for="message">Message <span class="text-danger">*</span></label>
+										<div class="input-group">
+											<div class="input-group-prepend">
+												<div class="input-group-text">
+													<i class="fa fa-comment" aria-hidden="true"></i>
+												</div>
+											</div>
+											<textarea class="form-control" rows="5" id="message" name="message"
+														 placeholder="Message (2000 characters max)"></textarea>
+										</div>
+									</div>
+									<!-- reCAPTCHA -->
+									<div class="g-recaptcha"
+										  data-sitekey="6Le7CrMUAAAAAEruOvxU3qaGg2FwkQ2ntcLV6bAq"></div>
+									<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send
+									</button>
+									<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset
+									</button>
+								</form>
+							</div>
+							<!--empty area for form error/success output-->
+							<div class="row">
+								<div class="col-xs-12">
+									<div id="output-area"></div>
+								</div>
 
+						</section>
+						<!--Bootstrap sticky footer-->
+						<footer>
+							<div class="container-fluid">
+								<div class="sticky-footer text-center text-white bg-dark pt-2 pb-0 m-0 fixed-bottom">
+									<p>
+										<a href="https://github.com/chrystalcopeland/" target="_blank"><i
+												class="fab fa-github fa-2x"></i></a>
+										<a href="https://www.linkedin.com/in/copelandchrystal/" target="_blank"><i
+												class="fab fa-linkedin fa-2x"></i></a>
+										<a href="https://twitter.com/chryscopeland/" target="_blank"><i
+												class="fab fa-twitter fa-2x"></i></a>
+										<br>
+										Chrystal Copeland ©2019
+									</p>
+								</div>
+							</div>
+						</footer>
+		</main>
 
 	</body>
 </html>
