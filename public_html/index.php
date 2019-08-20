6c5de4b23cdf4f845ce5ccf8f76f0fe804ed3d09
Chrystal Copeland <canderson73@cnm.edu>
2019-08-20 16:52:07 -0600

    added font awesome

--- public_html/index.php
+++ public_html/index.php
@@ -11,6 +11,9 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 				integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+		<!-- Font Awesome -->
+		<script src="https://kit.fontawesome.com/d3c857c1fb.js"></script>
+
 		<!-- Google Font -->
 		<link href="https://fonts.googleapis.com/css?family=Sacramento&display=swap" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Roboto:300&display=swap" rel="stylesheet">
