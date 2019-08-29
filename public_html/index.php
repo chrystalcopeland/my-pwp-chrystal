08bb0704336a791b97e6871874500a9653d53784
Chrystal Copeland <canderson73@cnm.edu>
2019-08-29 05:40:42 -0600

    added text and removed unessesary CSS

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<!-- JQuery Library -->
-		<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+				  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
@@ -58,12 +59,12 @@
 				</button>
 				<h2 class="navbar-brand brand-name">
 					<a href="index.html"><img class="img-responsive2"
-													  src="images/dahlia-grey.png" height="50 px" width="50 px"></a>
+													  src="images/dahlia-grey.png" alt="dahlia logo" height="50" width="50"></a>
 				</h2>
 				<div class="collapse navbar-collapse" id="navbarNav">
 					<ul class="navbar-nav">
 						<li class="nav-item active">
-							<a class="nav-link" href="#wellness">Why Wellness?a</a>
+							<a class="nav-link" href="#wellness">Why Wellness?</a>
 						</li>
 						<li class="nav-item active">
 							<a class="nav-link" href="#about">About</a>
@@ -76,7 +77,6 @@
 						</li>
 					</ul>
 				</div>
-
 			</nav>
 			<!--Hero image and welcome message-->
 			<section>
@@ -86,25 +86,17 @@
 							<div class="container pt-5">
 								<h1 class="display-3 text-center font-weight-bold">Chrystal Copeland</h1>
 								<p class="lead text-center" id="subtext">Making the internet a healthier place to be, one
-									website at
-									a time.</p>
+									website at a time.</p>
 							</div>
 							<div class="flex-container-1">
-								<img class="img-fluid d-block mx-auto" src="images/dahlia-white.png" height="200" width="200">
+								<img class="img-fluid d-block mx-auto" src="images/dahlia-white.png" alt="white dahlia logo"
+									  height="200" width="200">
 							</div>
 						</div>
-						<ul class="flex-container-2" id="wellness-businesses">
-							<ol class="flex-item">Yoga</ol>
-							<ol class="flex-item">Massage</ol>
-							<ol class="flex-item">Acupuncture</ol>
-							<ol class="flex-item">Chiropractic</ol>
-							<ol class="flex-item">Fitness</ol>
-							<ol class="flex-item">Nutrition</ol>
-						</ul>
 					</div>
 				</div>
 			</section>
-			<!-- Wellness Clients-->
+			<!-- What is Wellness-->
 			<section class="py-5">
 				<div class="container" id="wellness">
 					<h2>What is Wellness?</h2>
@@ -122,7 +114,8 @@
 								wellness.
 							</p>
 							<div id="floated">
-								<img src="images/globalwellnesseconomy2017.png" height="400px">
+								<img src="images/globalwellnesseconomy2017.png" alt="wellness infographic" height="400"
+									  width="500">
 							</div>
 							<p>
 								With an ever growing demand and increasing diversity in products, services, and tourism within
@@ -135,17 +128,11 @@
 							<p>
 								Additionally, with a worth of $4.2 trillion dollars, the wellness industry is proving to be a
 								market worth targeting. For marketing and web development companies this means adding
-								health and wellness experts to development teams who are also tech savy.
-								<aside class="pquote">
-									<blockquote>
+								health and wellness experts to development teams who are also tech savvy.
 							<p>Web developers with backgrounds as wellness experts can speak
-								the language of both the "wellness-chic and the development-geek". </p>
-							<p>This fosters effective
+								the language of both the "wellness-chic and the development-geek". This fosters effective
 								communication, client loyalty, healthy communication and,
-								ultimately, happy clients with healthy profits. - Chrystal Copeland</p>
-							</blockquote>
-							</aside>
-							</p>
+								ultimately, happy clients with healthy profits.</p>
 						</div>
 					</div>
 				</div>
@@ -163,14 +150,14 @@
 							<p class="lead">Chrystal Copeland has been part of the health and wellness field for over 10 years.
 								She has been an active wellness advocate and massage therapist. She strives to inspire and
 								encourage
-								healthy lifestyle choices in the lives of her clients and the lives of those around her.
+								healthy lifestyle choices in the lives of those around her.
 							</p>
 
 							<p>She was the founder and owner of Tree of Life Massage and Wellness in Albuquerque, New Mexico.
 								She began creating websites for her own business, which led to creating and consulting on
 								websites for other wellness businesses in the area. She began learning wordpress and created
 								ABQpregnancy.com, a resource directory listing for expecting families in Albuquerque, New
-								Mexico. She also volunteerd to create websites for her son's boy scout troop and re-vamped the
+								Mexico. She also volunteered to create websites for her son's boy scout troop and re-vamped the
 								brand and website for a local non-profit foundation, The Youth Jazz Collaborative of New
 								Mexico.</p>
 							<p>
@@ -179,27 +166,28 @@
 								Chrystal decided to take the leap, or dive so to speak, to learn more about web development.
 								She enrolled in CNM's Deep Dive Coding Bootcamp in 2019.
 							<p>
-								During her time in the bootcamp, Chrystal has been part of the ABQ Bike Trails team. The group
-								developed a website and app to highlight safe trails that are bike friendly in Albuquerque.
-								The team developed the website using PHP in PhpStorm, a MySQL database, CSS, Javascript, and
-								other languages ******** add additional details of the program here.
+								During her time in the Deep Dive Bootcamp, Chrystal has been part of the ABQ Bike Trails team. The group
+								developed a website application to highlight safe trails that are bike friendly in Albuquerque.
+								The team developed the website using PHP in PhpStorm, MySQL, CSS, Javascript, and React. They
+								used Mapbox to integrate their data from the city of Albuquerque.
 							</p>
 							<p>
-								Chrystal's greatest take away from the Bootcamp was the great satisfaction she recieved from
+								Chrystal's greatest take away from the Bootcamp was the deep satisfaction she received from
 								working in a team. After being a solo entrepreneur for a decade, she found working in a team
 								environment incredibly rewarding. Together, the team was able to accomplish a large task in a
-								short amount of time. Each team member brought stregths to the team, making the group as a
+								short amount of time. Each team member brought strengths to the team, making the group as a
 								whole stronger than the individual.
 							</p>
 						</div>
 					</div>
+				</div>
 			</section>
 			<!-- portfolio images -->
-			<!-- ABQ Bikes -->
 			<section class="py-5">
 				<div class="container" id="portfolio">
 					<h2>Portfolio</h2>
 					<div class="row">
+						<!-- ABQ Bikes -->
 						<div class="col-lg-3 col-sm-6">
 							<img class="img-fluid rounded d-block mx-auto mb-4"
 								  src="images/bike-abq.png" alt="ABQ Bike Trails">
@@ -208,19 +196,23 @@
 									<h5 class="card-title">ABQ Bike Trails</h5>
 									<p class="card-text">Bike trails resource built from scratch in a collaborative effort
 										using
-										Agile programming in LAMP stack. </p>
+										Agile programming in LAMP stack.
+									</p>
+									<a href="https://www.abqbiketrails.com"><button type="button" class="btn btn-outline-info">ABQ Bike Trails </button></a>
 								</div>
 							</div>
 						</div>
 						<!-- Tree of Life Massage -->
 						<div class="col-lg-3 col-sm-6">
 							<img class="img-fluid rounded d-block mx-auto mb-4"
-								  src="images/tree-of-life-logo.jpg" , alt="Tree of Life Massage">
+								  src="images/tree-of-life-logo.jpg" alt="Tree of Life Massage">
 							<div class="card mb-4">
 								<div class="card-body">
 									<h5 class="card-title">Tree of Life Massage</h5>
 									<p class="card-text">Health and Wellness website for Tree of Life Massage using
-										WIX.</p>
+										WIX.
+									</p>
+									<a href="https://www.treeoflifemassage.com"><button type="button" class="btn btn-outline-info" >Tree of Life Massage</button></a>
 								</div>
 							</div>
 						</div>
@@ -233,6 +225,7 @@
 									<h5 class="card-title">ABQpregnancy.com</h5>
 									<p class="card-text">Resource directory website built using Wordpress and Divi
 										theme.</p>
+									<a href="https://www.abqpregnancy.com"><button type="button" class="btn btn-outline-info">ABQpregnancy.com</button></a>
 								</div>
 							</div>
 						</div>
@@ -243,8 +236,10 @@
 							<div class="card mb-4">
 								<div class="card-body">
 									<h5 class="card-title">Youth Jazz Collborative</h5>
-									<p class="card-text">Rebranding and new website for small non-profit in New Mexico
-										using Squarespace.</p>
+									<p class="card-text">Branding and new website for small non-profit in New Mexico
+										using Squarespace.
+									</p>
+									<a href="https://youthjazznm.org/"><button type="button" class="btn btn-outline-info">Youth Jazz Collaborative</button></a>
 								</div>
 							</div>
 						</div>
@@ -292,7 +287,8 @@
 													<i class="fa fa-pencil" aria-hidden="true"></i>
 												</div>
 											</div>
-											<input type="text" class="form-control" id="contactDemoSubject" name="contactDemoSubject"
+											<input type="text" class="form-control" id="contactDemoSubject"
+													 name="contactDemoSubject"
 													 placeholder="Subject">
 										</div>
 									</div>
@@ -304,7 +300,8 @@
 													<i class="fa fa-comment" aria-hidden="true"></i>
 												</div>
 											</div>
-											<textarea class="form-control" rows="5" id="contactDemoMessage" name="contactDemoMessage"
+											<textarea class="form-control" rows="5" id="contactDemoMessage"
+														 name="contactDemoMessage"
 														 placeholder="Message (2000 characters max)"></textarea>
 										</div>
 									</div>
@@ -322,15 +319,16 @@
 								<div class="col-xs-12">
 									<div id="output-area"></div>
 								</div>
+							</div>
 						</section>
 					</div>
 				</div>
 			</section>
 
 			<!--Bootstrap sticky footer-->
-			<footer class="sticky-footer text-center text-white bg-dark pt-2 pb-0 fixed-bottom">
+			<footer>
 				<div class="container-fluid">
-					<div>
+					<div class="sticky-footer text-center text-white bg-dark pt-2 pb-0 fixed-bottom">
 						<p>
 							<a href="https://github.com/chrystalcopeland/" target="_blank"><i
 									class="fab fa-github fa-2x"></i></a>
